d9fb404328379dfe3450103c6c51e520c6bc9829
Dev-Songkran <cronozen@gmail.com>
2023-02-21 16:49:33 +0700

    feat: 🎸 auto install pkg success

--- bin/createExpressServer.js
+++ bin/createExpressServer.js
@@ -3,7 +3,10 @@
 const path = require("path");
 const commander = require("commander");
 const spawn = require("cross-spawn");
-const fs = require("fs");
+const fs = require("fs-extra");
+const chalk = require("chalk");
+
+let projectName;
 
 const init = () => {
   const program = new commander.Command("service-express")
@@ -15,9 +18,11 @@ const init = () => {
     })
     .allowUnknownOption()
     .parse(process.argv);
+
+  createServer(projectName);
 };
 
-const createServer = ({ name, verbose, scriptsVersion, template }) => {
+const createServer = (name) => {
   const root = path.resolve(name);
   const appName = path.basename(root);
 
@@ -38,7 +43,7 @@ const createServer = ({ name, verbose, scriptsVersion, template }) => {
 
   fs.writeFileSync(
     path.join(root, "package.json"),
-    JSON.stringify(packageJson, null, 2) + os.EOL
+    JSON.stringify(packageJson, null, 2)
   );
 
   process.chdir(root);
@@ -54,6 +59,7 @@ const createServer = ({ name, verbose, scriptsVersion, template }) => {
 
   const src = path.join(root, "src");
   fs.mkdirSync(src, { recursive: true });
+  fs.writeFileSync(path.join(root, ".env"), "");
 
   dirs.map((dir) => {
     if (!!dir.endsWith(".ts")) {
@@ -106,6 +112,79 @@ const createServer = ({ name, verbose, scriptsVersion, template }) => {
   });
 
   console.log("🎉 Success installation.");
+  console.log(`cd ${name}`);
 };
+
+function isSafeToCreateProjectIn(root, name) {
+  const validFiles = [
+    ".DS_Store",
+    ".git",
+    ".gitattributes",
+    ".gitignore",
+    ".gitlab-ci.yml",
+    ".hg",
+    ".hgcheck",
+    ".hgignore",
+    ".idea",
+    ".npmignore",
+    ".travis.yml",
+    "docs",
+    "LICENSE",
+    "README.md",
+    "mkdocs.yml",
+    "Thumbs.db",
+  ];
+  // These files should be allowed to remain on a failed install, but then
+  // silently removed during the next create.
+  const errorLogFilePatterns = [
+    "npm-debug.log",
+    "yarn-error.log",
+    "yarn-debug.log",
+  ];
+  const isErrorLog = (file) => {
+    return errorLogFilePatterns.some((pattern) => file.startsWith(pattern));
+  };
+
+  const conflicts = fs
+    .readdirSync(root)
+    .filter((file) => !validFiles.includes(file))
+    // IntelliJ IDEA creates module files before CRA is launched
+    .filter((file) => !/\.iml$/.test(file))
+    // Don't treat log files from previous installation as conflicts
+    .filter((file) => !isErrorLog(file));
+
+  if (conflicts.length > 0) {
+    console.log(
+      `The directory ${chalk.green(name)} contains files that could conflict:`
+    );
+    console.log();
+    for (const file of conflicts) {
+      try {
+        const stats = fs.lstatSync(path.join(root, file));
+        if (stats.isDirectory()) {
+          console.log(`  ${chalk.blue(`${file}/`)}`);
+        } else {
+          console.log(`  ${file}`);
+        }
+      } catch (e) {
+        console.log(`  ${file}`);
+      }
+    }
+    console.log();
+    console.log(
+      "Either try using a new directory name, or remove the files listed above."
+    );
+
+    return false;
+  }
+
+  // Remove any log files from a previous installation.
+  fs.readdirSync(root).forEach((file) => {
+    if (isErrorLog(file)) {
+      fs.removeSync(path.join(root, file));
+    }
+  });
+  return true;
+}
 
 module.exports = { init };
